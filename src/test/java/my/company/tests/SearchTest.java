7dac04288a6d30587161e5502bb15b6dc392278c
Aaftakhov <simple1elf@gmail.com>
2017-06-30 23:58:38 +0500

    selenoid

--- src/test/java/my/company/tests/SearchTest.java
+++ src/test/java/my/company/tests/SearchTest.java
@@ -1,15 +1,19 @@
 package my.company.tests;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
+//import io.github.bonigarcia.wdm.ChromeDriverManager;
 import my.company.steps.WebDriverSteps;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+//import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import ru.yandex.qatools.allure.annotations.Issue;
+import org.testcontainers.containers.BrowserWebDriverContainer;
 
 import java.net.URL;
 import java.util.HashMap;
@@ -35,8 +39,26 @@ public class SearchTest {
         return options;
     }
 
+    @Rule
+    public BrowserWebDriverContainer chrome =
+            new BrowserWebDriverContainer()
+                    .withDesiredCapabilities(DesiredCapabilities.chrome());
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        RemoteWebDriver driver = chrome.getWebDriver();
+        steps = new WebDriverSteps(driver);
+        //WebDriverRunner.setWebDriver(driver);
+    }
+
+    @After
+    public void tearDown() {
+        //WebDriverRunner.closeWebDriver();
+    }
+
+
+    //@Before
+    public void setUp1() throws Exception {
         //ChromeDriverManager.getInstance().setup();
 
         WebDriver webDriver;
@@ -45,7 +67,7 @@ public class SearchTest {
         capability.setCapability(ChromeOptions.CAPABILITY, getChromeOptions());
         //capability.setCapability("version", "58.0");
         capability.setCapability("screenResolution", "1920x1080x24");
-        capability.setCapability("enableVNC", true);
+        //capability.setCapability("enableVNC", true);
 
         webDriver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), capability);
         System.out.println("RemoteWebDriver started!!! ");
