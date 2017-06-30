44c40626257e8d2b1a049c20b7156deec3478451
Aaftakhov <simple1elf@gmail.com>
2017-06-30 13:04:18 +0500

    selenoid

--- src/test/java/my/company/tests/SearchTest.java
+++ src/test/java/my/company/tests/SearchTest.java
@@ -4,8 +4,16 @@ import io.github.bonigarcia.wdm.ChromeDriverManager;
 import my.company.steps.WebDriverSteps;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import ru.yandex.qatools.allure.annotations.Issue;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Dmitry Baev charlie@yandex-team.ru
@@ -15,10 +23,33 @@ public class SearchTest {
 
     private WebDriverSteps steps;
 
+    private static ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        //options.addArguments("--start-maximized");
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", prefs);
+
+        return options;
+    }
+
     @Before
     public void setUp() throws Exception {
-        ChromeDriverManager.getInstance().setup();
-        steps = new WebDriverSteps(new ChromeDriver());
+        //ChromeDriverManager.getInstance().setup();
+
+        WebDriver webDriver;
+
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        capability.setCapability(ChromeOptions.CAPABILITY, getChromeOptions());
+        capability.setCapability("screenResolution", "1920x1080x24");
+        //capability.setCapability("enableVNC", "true");
+
+        webDriver = new RemoteWebDriver(new URL("http://46.101.241.43:4444/wd/hub"), capability);
+        System.out.println("RemoteWebDriver started!!! ");
+
+        steps = new WebDriverSteps(webDriver);
     }
 
     @Test
